c9970bc572a54fe69a9fe56f625ba3c8de2747e5
Carlos Ramos <74577681+DevCarlosRamos@users.noreply.github.com>
2025-05-04 05:48:24 -0500

    cambios con mensajeria

--- server.js
+++ server.js
@@ -2,19 +2,31 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 3000 });
 
 let players = {};
+let idCounter = 1;
 
-wss.on('connection', function connection(ws) {
-  const id = Date.now();
-  players[id] = { x: 100, y: 100, color: getRandomColor() };
+function broadcast(msg) {
+  const data = JSON.stringify(msg);
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+}
+
+wss.on('connection', (ws) => {
+  const id = idCounter++;
+  players[id] = { x: 50, y: 50 };
 
   ws.send(JSON.stringify({ type: 'init', id, players }));
 
-  ws.on('message', function incoming(message) {
+  ws.on('message', (message) => {
     const data = JSON.parse(message);
     if (data.type === 'move') {
-      players[id].x = data.x;
-      players[id].y = data.y;
+      players[id] = { x: data.x, y: data.y };
       broadcast({ type: 'update', id, x: data.x, y: data.y });
+    }
+    if (data.type === 'chat') {
+      broadcast({ type: 'chat', id, message: data.message });
     }
   });
 
@@ -24,17 +36,5 @@ wss.on('connection', function connection(ws) {
   });
 });
 
-function broadcast(data) {
-  const msg = JSON.stringify(data);
-  wss.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(msg);
-    }
-  });
-}
-
-function getRandomColor() {
-  return '#' + Math.floor(Math.random()*16777215).toString(16);
-}
-
 console.log('Servidor WebSocket en ws://localhost:3000');
+
